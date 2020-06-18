a45678cae52b73c21758f6a35014fb6b4ea16d63
Ryan Moulton <ryanmoulton116@gmail.com>
2020-06-18 11:03:45 -0700

    complete app

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form v-on:submit="addTodo">
+      <input v-model="title" type="text" name="title" placeholder="Add todo..." />
+      <input class="btn" type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        id: Math.floor(Math.random() * 999) + 1,
+        title: this.title,
+        completed: false
+      };
+      // Send to parent
+      this.$emit("add-todo", newTodo);
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <TodoItem v-on:del-todo="$emit('del-todo', todo.id)" v-bind:todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+</style>
